7fc5b019144bd8c095753388ff5979e079bd80d0
humblewolfstudio <humblewolfstudio@gmail.com>
2023-03-07 20:13:33 +0100

    added changing background color by time of day

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,32 @@ import TodayForecast from './components/TodayForecast/TodayForecast';
 import { Forecast, ForecastContextInterface } from './types';
 import WeekForecast from './components/WeekForecast/WeekForecast';
 
+import chroma from 'chroma-js';
+
 export const ForecastContext = createContext<ForecastContextInterface | null>(null);
+const f = chroma.scale(['#1C2955ff', '#2F3B65ff', '#474F74ff', '#91C1D5ff', '#0077CBff', '#0077CBff', '#91C1D5ff', '#474F74ff', '#2F3B65ff', '#1C2955ff']);
 var alreadyMadeRequest = false;
+
+const setBackgroundColor = (color: string) => {
+  console.log(color)
+  document.documentElement.style.setProperty('--background-color', color);
+}
+
+const calculateBackgroundColorByHour = () => {
+  var hour = new Date().getHours();
+  var aux = (hour * 100) / 24 / 100;
+  console.log(aux)
+  console.log(f(aux).toString())
+  setBackgroundColor(f(aux).toString());
+}
+
 function App() {
 
   const [data, setData] = useState<Forecast>();
 
   useEffect(() => {
     if (!alreadyMadeRequest) {
+      calculateBackgroundColorByHour();
       console.log('petition')
       fetch("https://api.open-meteo.com/v1/forecast?latitude=41.392583&longitude=2.0001067&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
         .then((response) => response.json())
@@ -25,7 +43,7 @@ function App() {
         });
       alreadyMadeRequest = true;
     }
-
+    //setBackgroundColor('#ffffff')
   })
 
   return (
